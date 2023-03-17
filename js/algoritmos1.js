170c66eeb149f0dfd26db6a086d3551dfbba3405
cris <cristina94m@gmail.com>
2023-03-17 19:53:22 +0100

    tres primeros terminados

--- js/algoritmos1.js
+++ js/algoritmos1.js
@@ -1,4 +1,55 @@
-const randomNumbers = (max, min) => {
-    let random = Math.floor(Math.random() * (max - min + 1) + min);
-    return random;
+const e1a1 = () => {
+  let month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let actualDate = new Date();
+  let actualMonth = month[actualDate.getMonth()];
+  console.log(`Currently month is ${actualMonth}`);
+
+  let month31days = [
+    "January",
+    "March",
+    "May",
+    "July",
+    "August",
+    "October",
+    "December",
+  ];
+  let month30days = ["April", "June", "September", "November"];
+
+  if (month31days.includes(actualMonth)) {
+    console.log(`${actualMonth} has 31 days`);
+  } else if (month30days.includes(actualMonth)) {
+    console.log(`${actualMonth} has 30 days`);
+  } else if (actualMonth == "February") {
+    console.log(`${actualMonth} has 28 days`);
+  } else {
+    console.log("error");
+  }
 };
+
+
+const e6a1 = () => {
+  let email = "tolkien@lordofrings.com";
+  let password = "aNBR6ZeWPY";
+  let userEmail = prompt("Write here your email: ");
+  let userPassword = prompt("Write your password: ");
+
+  if (userEmail == email && userPassword == password) {
+    console.log("Welcome Mr.Tolkien");
+  } else {
+    console.log("Wrong password or email");
+  }
+};
+
--- js/algoritmos2.js
+++ js/algoritmos2.js
@@ -1,4 +1,30 @@
-const randomNumbers = (max, min) => {
+/*const randomNumbers = (max, min) => {
     let random = Math.floor(Math.random() * (max - min + 1) + min);
     return random;
+};*/
+
+const e1a2 = () => {
+  let word = "palindrome";
+  console.log(Array.from(word));
 };
+
+const e5a2 = () => {
+  const all = () => {
+    console.log("Welcome to text statistics program");
+    let phrase = prompt("Write here your sentence")
+    
+    let completeLength = phrase.length;
+    console.log(`Length: ${completeLength} characters`);
+    
+    let noSpaces = phrase.replace(/\s/g);
+
+    let words = "";
+    let unWords = "";
+    let shortest = "";
+    let longest = "";
+  };
+
+  all();
+};
+
+e5a2();

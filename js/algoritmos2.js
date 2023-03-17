6d8c6a3d031cf0f4d5dec25938002748710f647a
Alex Ruiz <aruizmartin87@gmail.com>
2023-03-17 19:54:40 +0100

    primer commit javascript

--- js/algoritmos2.js
+++ js/algoritmos2.js
@@ -2,3 +2,34 @@ const randomNumbers = (max, min) => {
     let random = Math.floor(Math.random() * (max - min + 1) + min);
     return random;
 };
+
+/* 5. Ejercicio
+La segunda opción pondrá la primera letra 
+de cada palabra en mayúscula, y la tercera 
+cambiará los
+espacios por comas. No puede haber 2 comas seguidas.
+El menú aceptará únicamente y para sí o n para no. 
+Almacenará las respuestas en un objeto con las
+propiedades: replaceChars, capitalize y replaceSpaces, 
+cuyos valores asociados serán booleanos.
+La salida del programa deberá mostrar el string 
+original, y a continuación el string con cada una de
+las modificaciones requeridas. Un ejemplo con todas 
+las opciones marcadas como true:
+• Lorem ipsum dolor sit amet
+• L0r3m 1p$um d0l0r $1t @m3t
+• Lorem Ipsum Dolor Sit Amet
+• Lorem,ipsum,dolor,sit,amet
+** El programa deberá registrar el tiempo de 
+realización, es decir, desde que comienza el proceso de
+transformación del string hasta que termina. 
+Después de los string requeridos, el programa mostrará
+las siguientes líneas: **
+• 5 words, 25 chars
+• work done in 750 miliseconds */
+
+const a2e5 = () => {
+
+
+
+}
--- js/algoritmos3.js
+++ js/algoritmos3.js
@@ -2,3 +2,37 @@ const randomNumbers = (max, min) => {
     let random = Math.floor(Math.random() * (max - min + 1) + min);
     return random;
 };
+
+/* 2. Ejercicio
+Escribe un programa que recoge la hora del sistema, 
+y al cargar la página pregunta al usuario su
+nombre. A continuación, muestra un saludo 
+personalizado (alerta) en función de la hora, 
+teniendo
+en cuenta los siguientes rangos:
+• Entre las 5:00 y las 11:59 → 
+“Good Morning, {userName}!”
+• Entre las 12:00 y las 17:59 → 
+“Good Afternoon, {userName}!”
+• Entre las 18:00 y las 04:59 → 
+“Good Evening, {userName}!”
+Los saludos deberán estar predefinidos en un objeto 
+con el identificador “greeting”. */
+
+const a3e7 = () => {
+
+
+
+}
+
+/* 7. Ejercicio
+Escribe un programa que genera 100 números aleatorios, 
+entre 0 y 500, y los almacena en un array.
+A continuación filtra todos los números impares, 
+ordenando los pares de mayor a menor. */
+
+const a3e7 = () => {
+
+
+
+}

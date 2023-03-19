1476ef636503f3359ce3d6cc5ae25bf6b57711e2
daniel <danielcgonze@gmail.com>
2023-03-19 12:28:00 +0100

    e2a2 terminado

--- js/algoritmos2.js
+++ js/algoritmos2.js
@@ -1,4 +1,22 @@
-const randomNumbers = (max, min) => {
-    let random = Math.floor(Math.random() * (max - min + 1) + min);
-    return random;
+e2a2 = () => {
+  let quote = "No lemon, no melon";
+  console.log(quote);
+
+  // he creado una función con mi código que convierte una frase
+  // en un array, invierte el orden de las letras y las vuelve a 
+  // convertir en un string sin comas separando los elementos.
+
+  function letterReverse() {
+    let arrayQuote = Array.from(quote);
+    console.log(arrayQuote);
+
+    let reverse = arrayQuote.reverse();
+    console.log(reverse);
+
+    let solution = reverse.join("");
+    console.log(solution);
+  }
+
+  letterReverse();
 };
+

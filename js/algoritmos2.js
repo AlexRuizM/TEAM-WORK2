cc886c9ecbe26e9dc62c197f899c292be22b8c69
RoarCriss <cristina94m@gmail.com>
2023-03-23 16:09:56 +0100

    actualizacion algoritmos javascript

--- js/algoritmos2.js
+++ js/algoritmos2.js
@@ -11,20 +11,57 @@ const e1a2 = () => {
 const e5a2 = () => {
   const all = () => {
     console.log("Welcome to text statistics program");
-    let phrase = prompt("Write here your sentence")
-    
+    let phrase = prompt("Write here your sentence");
+    console.log(phrase);
+
     let completeLength = phrase.length;
     console.log(`Length: ${completeLength} characters`);
-    
-    let noSpaces = phrase.replace(/\s/g);
 
-    let words = "";
-    let unWords = "";
-    let shortest = "";
-    let longest = "";
+    let noSpaces = phrase.replace(/\s+/g, "").length;
+    console.log(`Length without white spaces: ${noSpaces}`);
+
+    let words = phrase.split(" ");
+    let wordsNumber = words.length;
+    console.log(`Words: ${wordsNumber}`);
+
+    let phraseArray = phrase.split(" ");
+    let unWords = new Set(phraseArray);
+    let countUnWords = unWords.size;
+    console.log(`Unique Words: ${countUnWords}`);
+
+    let unWordsPercentage = (countUnWords / wordsNumber) * 100;
+    console.log(`Unique Words(%): ${unWordsPercentage}%`);
+
+    function getShortestWord() {
+      let shortest = 0;
+      for (let i = 0; i < phraseArray.length; i++) {
+        if (phraseArray[i].length < phraseArray[shortest].length) {
+          shortest = i;
+        }
+      }
+      return phraseArray[shortest];
+    }
+
+    let shortest = getShortestWord();
+    console.log(`Shortest Word: ${shortest}`);
+
+    function findLongestWord() {
+      var longest = "";
+
+      phraseArray.forEach(function (phraseArray) {
+        if (phraseArray.length > longest.length) {
+          longest = phraseArray;
+        }
+      });
+
+      return longest;
+    }
+    
+    let longest = findLongestWord();
+    console.log(`Longest Word: ${longest}`);
   };
 
   all();
 };
 
-e5a2();
+
--- js/algoritmos3.js
+++ js/algoritmos3.js
@@ -1,4 +1,26 @@
-const randomNumbers = (max, min) => {
+/*const randomNumbers = (max, min) => {
     let random = Math.floor(Math.random() * (max - min + 1) + min);
     return random;
-};
+};*/
+
+
+
+const e3a3 = () => {
+    const randomHexGenerator = () => {
+        let colorGenerator = (Math.random() * 0xfffff * 1000000).toString(16);
+        return '#' + colorGenerator.slice(0, 6);
+      };
+      
+      let hexCodes = [];
+      for (let i = 0; i < 10; i++) {
+        let codes = randomHexGenerator();
+      hexCodes.push(codes);
+      }
+      console.log(hexCodes);
+    
+      let randomIndex = Math.floor(Math.random() * hexCodes.length);
+      let randomCodeSelector = hexCodes[randomIndex];
+      console.log(randomCodeSelector);
+    
+      console.log("%cHello World!", "color: " + randomCodeSelector);
+}

a3f917d043a7c30bb702238fb38a6b2885f730eb
Daniel Castaño <125692802+Dan328x@users.noreply.github.com>
2023-04-15 14:23:12 +0200

    Feature dan328x (#5)
    * e2a1 terminado
    
    * e7a1 terminado
    
    * e2a2 terminado
    
    * e7a2 terminado
    
    * e4a3 terminado
    
    * e9a3 terminado cifrado César
    
    * algoritmos de Dani completados
    
    ---------
    
    Co-authored-by: Alex Ruiz <125692791+AlexRuizM@users.noreply.github.com>

--- js/algoritmos1.js
+++ js/algoritmos1.js
@@ -30,6 +30,47 @@ const e1a1 = () => {
   }
 };
 
+/* Ejercicio 2 */
+
+const e2a1 = () => {
+  // preguntamos al usuario por un mes a través del prompt:
+  let month = prompt("What month are we talking about?");
+
+  /* el programa me da el mensaje de error si el usuario no escribe 
+el mes exactamente igual, con la primera letra en mayúscula. 
+Para evitar el error quiero convertir la primera letra del string 
+a mayúsculas cuando el usuario la escriba en minúsculas: 
+    month[0] obtiene el primer caracter del string.
+    .toUpperCase() convierte la primera letra en mayúscula.
+    .slice(1) devuelve un nuevo string que comienza desde el segundo 
+    caracter, es decir, si month = "enero"  month2 = "Enero" */
+
+  let month2 = month[0].toUpperCase() + month.slice(1);
+
+  if (
+    month2 == "January" ||
+    month2 == "March" ||
+    month2 == "May" ||
+    month2 == "July" ||
+    month2 == "August" ||
+    month2 == "October" ||
+    month2 == "December"
+  ) {
+    console.log(month2 + ` has 31 days.`);
+  } else if (
+    month2 == "April" ||
+    month2 == "June" ||
+    month2 == "September" ||
+    month2 == "November"
+  ) {
+    console.log(month2 + ` has 30 days.`);
+  } else if (month2 == "February") {
+    console.log(month2 + ` has 28/29 days (depending on leap year.)`);
+  } else {
+    alert("That's not a real month!");
+  }
+};
+
 //Ejercicio 3, algoritmos 1//
 
 const e3a1 = () => {
@@ -129,6 +170,40 @@ const e6a1 = () => {
 //e6a1();
 */
 
+/* Ejercicio 7 */
+
+const e7a1 = () => { 
+
+let moneda = prompt(
+  "Escriba qué conversión quiere realizar: Dólar americano, Yen japonés, Libra esterlina o Franco suizo."
+);
+let cantidad = prompt("Escriba la cantidad a convertir (en euros:)");
+let dolar = cantidad * 1.061;
+let yen = cantidad * 139.914;
+let libra = cantidad * 0.871;
+let franco = cantidad * 0.979;
+
+switch (moneda) {
+  case "Dólar americano":
+    console.log(cantidad, `euros equivalen a `, dolar, ` dólares americanos.`);
+    break;
+  case "Yen japonés":
+    console.log(cantidad, `euros equivalen a `, yen, ` yen japoneses.`);
+    break;
+  case "Libra esterlina":
+    console.log(cantidad, `euros equivalen a `, libra, ` libras esterlinas.`);
+    break;
+  case "Franco suizo":
+    console.log(cantidad, `euros equivalen a `, franco, ` francos suizos.`);
+    break;
+
+  default:
+    console.log("Por favor, introduzca una conversión válida.");
+    break;
+}
+
+}
+
 //Ejercicio 8 algoritmos  1//
 
 const e8a1 = () => {
@@ -167,6 +242,7 @@ const e8a1 = () => {
 //e8a1();
 
 /* Ejercicio 9 */
+
 const e9a1 = () => {
   let document = prompt("Introduzca su DNI/NIE");
   let expresionRegularNie = /^[XT]\d{7}[A-Z]$/;
@@ -182,6 +258,7 @@ const e9a1 = () => {
 };
 
 /* Ejercicio 10 */
+
 const a1e10 = () => {
   let num1 = randomNumbers(6, 1);
   let num2 = randomNumbers(6, 1);
--- js/algoritmos3.js
+++ js/algoritmos3.js
@@ -106,6 +106,125 @@ const e3a3 = () => {
   console.log("%cHello World!", "color: " + randomCodeSelector);
 };
 
+/* Ejercicio 4 */
+
+e4a3 = () => {
+  // Lo primero que necesitamos es que el usuario introduzca su DNI:
+  let dniUser = prompt("DNI Identification: ");
+  /*   console.log(dniUser); */
+  // Para poder validar el DNI, tenemos que quitarle la letra del final
+  // y dejar solo el número. Para ello vamos a convertir dniUser
+  // en un array y le vamos a quitar el último elemento:
+
+  let dniArray = Array.from(dniUser);
+  dniArray.pop();
+  /*   console.log(dniArray); */
+
+  // Ahora necesitamos que dniArray vuelva a ser un número o string:
+
+  let dniNumber = dniArray.join("");
+  /*   console.log(dniNumber); */
+
+  // Para validar el DNI tenemos que conseguir el resto de dividir el
+  // número entre 23:
+
+  let dniResto = dniNumber % 23;
+  /*   console.log(dniResto); */
+
+  // Una vez que tenemos el resto, hay que comprobar a qué letra de la tabla
+  // corresponde. Si el número (dniResto) coincide con su letra, el dni es válido.
+
+  let letter;
+
+  switch (dniResto) {
+    case 0:
+      letter = "T";
+      break;
+    case 1:
+      letter = "R";
+      break;
+    case 2:
+      letter = "W";
+      break;
+    case 3:
+      letter = "A";
+      break;
+    case 4:
+      letter = "G";
+      break;
+    case 5:
+      letter = "M";
+      break;
+    case 6:
+      letter = "Y";
+      break;
+    case 7:
+      letter = "F";
+      break;
+    case 8:
+      letter = "P";
+      break;
+    case 9:
+      letter = "D";
+      break;
+    case 10:
+      letter = "X";
+      break;
+    case 11:
+      letter = "B";
+      break;
+    case 12:
+      letter = "N";
+      break;
+    case 13:
+      letter = "J";
+      break;
+    case 14:
+      letter = "Z";
+      break;
+    case 15:
+      letter = "S";
+      break;
+    case 16:
+      letter = "Q";
+      break;
+    case 17:
+      letter = "V";
+      break;
+    case 18:
+      letter = "H";
+      break;
+    case 19:
+      letter = "L";
+      break;
+    case 20:
+      letter = "C";
+      break;
+    case 21:
+      letter = "K";
+      break;
+    case 22:
+      letter = "E";
+      break;
+  }
+
+  /* console.log(letter); */
+  // Una vez que tenemos la letra que corresponde al resto, tenemos
+  // que compararlo con el DNI original del usuario. Para ello vamos a
+  // poner la letra obtenida al final de nuestro dniArray. En caso de que el
+  // dniUser sea igual al dniCheck, el DNI será válido.
+
+  dniArray.push(letter);
+  dniValid = dniArray.join("");
+  /* console.log(dniCheck); */
+
+  if (dniValid == dniUser) {
+    console.log("Valid DNI.");
+  } else {
+    console.log("The data entered is wrong.");
+  }
+};
+
 /* Ejercicio 5 */
 const a5e3 = () => {
   let userQuestion = parseInt(prompt("¿Cuántas matrículas quieres generar?"));
@@ -444,4 +563,58 @@ const e8a3 = () => {
       }
     }
   } while (game == false);
+};
+
+/* Ejercicio 9 */
+
+e9a3 = () => {
+  // Quiero un programa que cifre o descifre un mensaje y muestre el contrario por la consola. Tiene que ser una función con dos parámetros, el primero para indicar si hay que cifrar o descifrar, y el segundo para el mensaje.
+  // Tengo que usar unicode y ASCII table. ASCII (American Standard Code for Information Interchange). En esa tabla, cada letra del alfabeto tiene un valor numérico.
+
+  // 1º Creamos una función cuyo parámetro es el mensaje que queremos cifrar.
+  // 2º Establecemos una variable que almacenará el resultado: encrypted.
+  // 3º Creamos un bucle for que recorra el mensaje.
+  // 4º Creamos una variable que almacene el mensaje, cambiando cada una de las letras o símbolos por su correspondiente valor numérico en la tabla ASCII, y le sumamos 3 posiciones (he añadido la variable rule para poder modificar el número de posiciones).
+  // 5º Nuestra variable encrypted ahora es igual a un string formado con los valores numéricos de la tabla ASCII. Sale un mensaje cifrado porque cada letra se ha desplazado 3 posiciones en la tabla y probablemente ya no tiene sentido el mensaje.
+  // 6º Pedimos a la función que nos devuelva la variable encrypted.
+  // 7º Repetimos el proceso a la inversa en otra función para descifrar en lugar de cifrar.
+
+  function encrypt(message) {
+    let encrypted = "";
+    rule = 3;
+    for (let start = 0; start < message.length; start++) {
+      let code = message[start].charCodeAt(0) + rule;
+      encrypted += String.fromCharCode(code);
+    }
+    return encrypted;
+  }
+  function decrypt(message) {
+    let decrypted = "";
+    rule = 3;
+    for (let start = 0; start < message.length; start++) {
+      let decode = message[start].charCodeAt(0) - rule;
+      decrypted += String.fromCharCode(decode);
+    }
+    return decrypted;
+  }
+
+  // Ya tenemos una función para cifrar y otra para descifrar. Ahora tenemos que crear una función con 2 parámetros. 
+  // El primero para indicar si hay que cifrar o descifrar el mensaje (true or false). El segundo para el texto o mensaje.
+
+  function cesarEncryption(cipher, message) {
+    let result = "";
+    if (cipher == true) {
+      result = encrypt(message);
+    } else {
+      result = decrypt(message);
+    }
+    return result;
+  }
+
+/* let message = "Hola Mundo!";
+let cipher = true;  */
+/* let message = "Krod#Pxqgr$";
+let cipher = false;  */
+
+  console.log(cesarEncryption(cipher, message));
 };
